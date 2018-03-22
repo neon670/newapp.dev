42e3b28ab1fcb0c5af4175cb43feb05298c6355a
noe nevarez <closner_nevarez@hotmail.com>
2018-03-22 21:11:15 +0700

    updates

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'app_db');
+  define('DB_PASS', 'password');
+  define('DB_NAME', 'newapp_db');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/newapp');
+  // Site Name
+  define('SITENAME', 'SharePost');
+  // App Version
+  define('APPVERSION', '1.0.0');
